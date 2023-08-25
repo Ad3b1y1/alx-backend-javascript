bd95ae07a5864927470eb2044953cf30fdbecf95
Ad3b1y1 <opeoluwaadebiyi@outlook.com>
2023-08-25 02:03:32 +0100

    Nodebasics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Adebiyi Adeyemi <https://github.com/Ad3b1y1>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
